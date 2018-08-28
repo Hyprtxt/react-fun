1b1bf613aa6fda57c1012f81c08d07fef3d8c823
Taylor <taylor@hyprtxt.com>
2018-08-27 20:11:12 -0700

    better with readme

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Hyperspace
+
+animejs smiley face effect in a react component.
+
+```jsx
+import Hyperspace from "@hyprtxt/react-fun";
+import Hyperspace from "@plunder/hyperspace";
+// Then JSX
+return <Hyperspace />;
+```
--- src/hyperspace/hyperspace.jsx
+++ src/hyperspace/hyperspace.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './hyperspace.css';
-import AnimeJS from 'animejs';
+import React from "react";
+import PropTypes from "prop-types";
+import "./hyperspace.css";
+import AnimeJS from "animejs";
 
 class Hyperspace extends React.Component {
   constructor(props) {
     super(props);
-    // this.setRef = this.setRef.bind(this);
+    this.state;
     this.myRef = React.createRef();
   }
 
   componentDidMount() {
-    // const { theAnimation } = this;
-    console.log('AnimeJS DidMount', this.myRef.current);
     let currentHue = 180;
     const hueCounter = () => {
       /* eslint-disable */
@@ -23,35 +21,30 @@ class Hyperspace extends React.Component {
       // console.log(currentHue);
       return currentHue;
     };
-    const numberOfEls = 180;
+    const numberOfEls = 50;
     const duration = 2000;
     const midScreenX = window.innerWidth / 2;
     const midScreenY = window.innerHeight / 2;
-    // const midScreenX = 100;
-    // const midScreenY = 100;
     const radius = Math.sqrt(midScreenX * midScreenX + midScreenY * midScreenY);
     const fragment = document.createDocumentFragment();
 
     for (let i = 0; i < numberOfEls; i++) {
       const hue = hueCounter();
-      // Math.round(360 / numberOfEls * i);
+      // const angle = Math.round(360 / numberOfEls * i);
       const angle = Math.random() * Math.PI * 2;
-      const el = document.createElement('div');
-      el.classList.add('particule');
+      const el = document.createElement("div");
+      el.appendChild(document.createTextNode("☺"));
+      el.classList.add("particule");
       el.style.color = `hsl(${hue}, 100%, 50%)`;
-      el.style.width = '0';
-      el.style.height = '0';
       AnimeJS({
         targets: el,
-        fontSize: ['10px', '230px'],
-        // width: ['1px', '10px'],
-        // height: ['1px', '10px'],
+        fontSize: ["10px", "230px"],
         left: [`${midScreenX}px`, `${Math.cos(angle) * radius + midScreenX}px`],
         top: [`${midScreenY}px`, `${Math.sin(angle) * radius + midScreenY}px`],
         delay: (duration / numberOfEls) * i,
         duration,
-        easing: 'easeInExpo',
-        loop: true,
+        easing: "easeInExpo",
+        loop: true
       });
       fragment.appendChild(el);
     }
@@ -59,18 +52,13 @@ class Hyperspace extends React.Component {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log(nextProps, nextState);
+    console.log("shouldComponentUpdate", nextProps, nextState);
     return false;
   }
 
-  componentWillUnmount() {
-    // const { theAnimation } = this;
-    // const $form = JQuery(theAnimation);
-    // $form.off('submit');
-  }
-
-  // setRef(domElement) {
-  //   this.theAnimation = domElement;
+  // componentWillUnmount() {
+  //   console.log( this.myRef.current )
+  //   const { theAnimation } = this;
   // }
 
   render() {
@@ -80,23 +68,11 @@ class Hyperspace extends React.Component {
 }
 
 Hyperspace.propTypes = {
-  children: PropTypes.any,
-  // className: PropTypes.string,
-  // disabled: PropTypes.bool,
-  // checked: PropTypes.bool,
-  // name: PropTypes.string,
-  // onChange: PropTypes.func,
+  children: PropTypes.any
 };
 
 Hyperspace.defaultProps = {
-  children: [],
-  // className: '',
-  // disabled: false,
-  // checked: true,
-  // name: 'name_me',
-  // onChange(e) {
-  //   console.log('Hyperspace value change', e);
-  // },
+  children: []
 };
 
 export default Hyperspace;
